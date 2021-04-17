95cba1f794d983ad0aaceac9e2b759e958b8f4e6
AgentMax05 <64991518+AgentMax05@users.noreply.github.com>
2021-04-17 01:49:10 -0400

    Create functions.hpp

---
+++ functions.hpp
@@ -0,0 +1,3 @@
+void display_board(std::vector<std::vector<int>> board);
+std::vector<std::vector<int>> make_move(int squre, int player, std::vector<std::vector<int>> board);
+bool check_win(int turn, std::vector<std::vector<int>> board);
